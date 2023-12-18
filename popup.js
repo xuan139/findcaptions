93242f7817c927fb8bea7887a0187c9a75b510b3
Xuan <13901996631@139.com>
2023-12-17 18:21:21 -0600

    update

--- popup.js
+++ popup.js
@@ -1,26 +1,65 @@
 // popup.js
 
 document.addEventListener('DOMContentLoaded', function() {
-  // Create a container div for your extension content
-  // var extensionContainer = document.createElement('div');
-  //  extensionContainer.innerHTML = '<div style="width: 10%;"><div id="result"></div><p></p><button id="getDataBtn">play</button></div>';
-
-  // // Append the container to the body of the webpage
-  // document.body.appendChild(extensionContainer);
 
   // Your existing code to get and display the current URL
-  // chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-  //   var url = tabs[0].url;
-  //   document.getElementById('url').textContent = 'URL: ' + url;
-  // });
-
-  // Add an event listener to the button for making the HTTP request
-  document.getElementById('getDataBtn').addEventListener('click', function() {
-    // URL for your HTTP request using the current tab's URL
+  var global_url = 'http://192.168.0.171:5000/'
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    var myImage = document.getElementById('myImage');
+    myImage.style.width = '150px'
+    myImage.src = global_url + 'static/demo.png'
+    // Set the src attribute dynamically
+    var currentTabUrl = tabs[0].url;
+    var regExp = /^.*(?:youtu.be\/|youtube.com\/(?:embed\/|v\/|.*v=|.*\/videos\/|.*[?&]v=))([^"&?\/\s]{11}).*$/;
+    // Extract the video ID using the regular expression
+    var match = currentTabUrl.match(regExp);
+    // Check if there is a match and return the video ID
+    var videoId = (match && match[1]) ? match[1] : null;
+  
+    var apiUrl = global_url+`/get_files/${videoId}`;
+    fetch(apiUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        // Replace 'yourdomain.com' with your actual domain or IP address
+        var baseUrl = global_url;
+  
+        var webpFiles = data.webp_files.map(file => baseUrl + file);
+        var vttFiles = data.vtt_files.map(file => baseUrl + file);
+  
+        console.log("WebP Files:", webpFiles);
+        console.log("VTT Files:", vttFiles);
+  
+        myImage.src = webpFiles[0];
+        
+        myImage.style.width = '300px'
+        // Create download links for each VTT file
+        var downloadLinks = document.getElementById('downloadVtt');
+        vttFiles.forEach((vttFile, index) => {
+          var link = document.createElement('a');
+          link.href = vttFile;
+          link.download = `caption${index + 1}.vtt`; // You can customize the file name
+          link.textContent = `Download VTT ${index + 1}`;
+          downloadLinks.appendChild(link);
+          downloadLinks.appendChild(document.createElement('br')); // Add line break
+        });
+        // Perform further actions with the complete file URLs as needed
+      })
+      .catch(error => {
+        console.error("Error fetching files:", error);
+      });
+  });
+  
+  
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    document.getElementById('result').innerHTML = 'generating captions backend...';
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       var currentTabUrl = tabs[0].url;
-      // var apiUrl = 'http://192.168.0.171:5000/get_youtube_video_info?youtube_url=' + encodeURIComponent(currentTabUrl);
-      var apiUrl = 'http://192.168.0.171:5000/searchvtt?youtube_url=' + encodeURIComponent(currentTabUrl);
+      var apiUrl = global_url+'searchvtt?youtube_url=' + encodeURIComponent(currentTabUrl);
            // Make the HTTP request using the fetch API
       fetch(apiUrl)
         .then(response => {
@@ -32,8 +71,6 @@ document.addEventListener('DOMContentLoaded', function() {
         /* The `.then(data => { ... })` block is a callback function that is executed after the HTTP
         request is successfully made and the response is received. */
         .then(data => {
-          // Display the raw response in the 'result' div
-          // document.getElementById('result').textContent =  data;
           document.getElementById('result').innerHTML = data.replace(/\n/g, '<br>');
         })
         .catch(error => {
@@ -42,4 +79,8 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
   });
+
 });
+
+
+// http://192.168.0.171:5000/get_files/https://www.youtube.com/watch?v=sVNrXXM1txo
